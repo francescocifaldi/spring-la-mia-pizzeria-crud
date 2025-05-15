a9b0df9c520309185077a406f7a85c7e998f3332
francescocifaldi <francesco.cifaldi@gmail.com>
2025-05-15 09:15:59 +0200

    ADD Model

---
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/model/pizza.java
@@ -0,0 +1,67 @@
+package org.lessons.java.spring_la_mia_pizzeria_crud.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "pizzas")
+public class pizza {
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank(message = "Name cannot be empty")
+    private String name;
+    private String description;
+    private String photo;
+    @NotBlank(message = "Price cannot be empty")
+    private Double price;
+
+    // Constructor
+    public pizza() {
+    }
+
+    // Getters and Setters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
